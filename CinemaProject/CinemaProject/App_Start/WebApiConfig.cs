dd411cafac411e6078283585b569e447d611ac2d
ramazan <ramazan.333a@gmail.com>
2016-03-02 23:47:37 +0500

    report for 2016-03-02

--- CinemaProject/CinemaProject/App_Start/WebApiConfig.cs
+++ CinemaProject/CinemaProject/App_Start/WebApiConfig.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Web.Http;
 
 namespace CinemaProject
@@ -14,6 +15,7 @@ namespace CinemaProject
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
 
             // Uncomment the following line of code to enable query support for actions with an IQueryable or IQueryable<T> return type.
             // To avoid processing unexpected or malicious queries, use the validation settings on QueryableAttribute to validate incoming queries.
