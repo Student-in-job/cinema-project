3a6a1547e4d705a9060f9f4a40a68fe928c588e2
ramazan <ramazan.333a@gmail.com>
2016-03-11 14:50:02 +0500

    changed identity models

--- OnlineCinemaProject/OnlineCinemaProject/Models/IdentityModels.cs
+++ OnlineCinemaProject/OnlineCinemaProject/Models/IdentityModels.cs
@@ -1,4 +1,5 @@
-﻿using System.Data.Entity;
+﻿using System;
+using System.Data.Entity;
 using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace OnlineCinemaProject.Models
@@ -6,6 +7,14 @@ namespace OnlineCinemaProject.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime? BirthDate { get; set; }
+        public string Email { get; set; }
+        public int? Sex { get; set; }
+        public DateTime? JoinDate { get; set; }
+        public decimal? Balance { get; set; }
+        public int? SubscriptionId { get; set; }
     }
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
@@ -16,4 +25,6 @@ namespace OnlineCinemaProject.Models
             Database.SetInitializer(new MySqlInitializer());
         }
     }
+
+    
 }
