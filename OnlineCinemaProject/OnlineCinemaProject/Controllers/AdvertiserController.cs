4b1ce8bad50bf2743d3dbbaa34a7e2b456592e72
ramazan <ramazan.333a@gmail.com>
2016-03-12 23:38:23 +0500

    added liliya's classes,views and models to our common project, naw she can work there

---
+++ OnlineCinemaProject/OnlineCinemaProject/Controllers/AdvertiserController.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using OnlineCinemaProject.Models;
+using PagedList;
+
+namespace OnlineCinemaProject.Controllers
+{
+    public class AdvertiserController : Controller
+    {
+        private readonly OnlineCinemaEntities _db = new OnlineCinemaEntities();
+
+        // GET: /Advertiser/
+        public ViewResult Index(string sortOrder, string currentFilter, string searchString, int? page)
+        {
+            ViewBag.CurrentSort = sortOrder;
+            ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            ViewBag.DateSortParm = sortOrder == "email" ? "email_desc" : "email";
+
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            ViewBag.CurrentFilter = searchString;
+
+            var advertisers = from s in _db.advertisers
+                              select s;
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                advertisers = advertisers.Where(s => s.name.ToUpper().Contains(searchString.ToUpper())
+                                       || s.email.ToUpper().Contains(searchString.ToUpper()));
+            }
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    advertisers = advertisers.OrderByDescending(s => s.name);
+                    break;
+                case "email":
+                    advertisers = advertisers.OrderBy(s => s.email);
+                    break;
+                case "email_desc":
+                    advertisers = advertisers.OrderByDescending(s => s.email);
+                    break;
+                default:  // Name ascending 
+                    advertisers = advertisers.OrderBy(s => s.name);
+                    break;
+            }
+
+            int pageSize = 3;
+            int pageNumber = (page ?? 1);
+            return View(advertisers.ToPagedList(pageNumber, pageSize));
+        }
+
+
+
+
+        // GET: /Advertiser/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            advertiser advertiser = _db.advertisers.Find(id);
+            if (advertiser == null)
+            {
+                return HttpNotFound();
+            }
+            return View(advertiser);
+        }
+
+        // GET: /Advertiser/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /Advertiser/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="id,name,email,phone_number")] advertiser advertiser)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.advertisers.Add(advertiser);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(advertiser);
+        }
+
+        // GET: /Advertiser/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            advertiser advertiser = _db.advertisers.Find(id);
+            if (advertiser == null)
+            {
+                return HttpNotFound();
+            }
+            return View(advertiser);
+        }
+
+        // POST: /Advertiser/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="id,name,email,phone_number")] advertiser advertiser)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Entry(advertiser).State = EntityState.Modified;
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(advertiser);
+        }
+
+        // GET: /Advertiser/Delete/5
+        public ActionResult Delete(bool? saveChangesError = false, int id = 0)
+        {
+            if (saveChangesError.GetValueOrDefault())
+            {
+                ViewBag.ErrorMessage = "Delete failed. Try again, and if the problem persists see your system administrator.";
+            }
+            advertiser advertiser = _db.advertisers.Find(id);
+            if (advertiser == null)
+            {
+                return HttpNotFound();
+            }
+            return View(advertiser);
+        }
+        
+        // POST: /Advertiser/Delete/5
+       [HttpPost]
+       [ValidateAntiForgeryToken]
+public ActionResult Delete(int id)
+{
+    try
+    {
+        advertiser advertiser = _db.advertisers.Find(id);
+        _db.advertisers.Remove(advertiser);
+        _db.SaveChanges();
+    }
+    catch (DataException/* dex */)
+    {
+        // uncomment dex and log error. 
+        return RedirectToAction("Delete", new {id, saveChangesError = true });
+    }
+    return RedirectToAction("Index");
+}
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ OnlineCinemaProject/OnlineCinemaProject/Controllers/Api/UserController.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.Results;
+using OnlineCinemaProject.Models;
+
+namespace OnlineCinemaProject.Controllers.Api
+{
+    public class UserController : ApiController
+    {
+        readonly OnlineCinemaEntities _db = new OnlineCinemaEntities();
+        [HttpGet]
+        [Route("api/user/{userId}/{tariffId}")]
+        public IHttpActionResult Subscribe(string userId, int tariffId)
+        {
+            var user = _db.aspnetusers.Find(userId);
+            var tariff = _db.tariffs.Find(tariffId);
+            if (tariff == null || user == null)
+            {
+                return NotFound();
+            }
+            if (DrawMoney(user,tariff.price))
+            {
+                return InternalServerError(new Exception("Ндостаточно средств на балансе"));
+            }
+            subscription subscription = new subscription
+            {
+                tariff = tariff,
+                payment = tariff.price,
+                start = DateTime.Now,
+                end = DateTime.Now
+            };
+            subscription.aspnetusers.Add(user);
+
+            _db.subscriptions.Add(subscription);
+            _db.SaveChanges();
+            return Ok();
+        }
+        [HttpGet]
+        [Route("api/user/{userId}/{amount}")]
+        public IHttpActionResult TopUpBalance(string userId, decimal amount)
+        {
+            var user = _db.aspnetusers.Find(userId);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            TopUp(user,amount);
+            return Ok();
+        }
+
+        public IHttpActionResult Watch(int movieId, string userId)
+        {
+            var movie = _db.movies.Find(movieId);
+            var user = _db.aspnetusers.Find(userId);
+            if (movie == null || user == null)
+            {
+                return NotFound();
+            }
+            var userMovies = user.usermovies.Where(i => i.movie_id == movieId);
+            if (true)
+            {
+
+            }
+            if (movie.price.Equals(0))
+            {
+                return NotFound();
+            }
+            else
+            {
+                if(CheckBalance(user.Balance,movie.price))
+                {
+
+                }
+            }
+            return NotFound();
+        }
+
+        public bool DrawMoney(aspnetuser user, decimal amount)// Снять деньги со счета клиента
+        {
+            if (CheckBalance((decimal)user.Balance, amount))
+            {
+                user.Balance = user.Balance - amount;
+                return true;
+            }
+            return false;
+        }
+
+        public void TopUp(aspnetuser user, decimal amount)
+        {
+            user.Balance += amount;
+        }
+
+        public bool CheckBalance(decimal balance, decimal amount ) // проверить хватит ли денег 
+        {
+            if (balance < amount)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
