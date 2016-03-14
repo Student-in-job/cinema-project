84905abd81f4ad5f7f2ea2f905a59d76edd42713
ramazan <ramazan.333a@gmail.com>
2016-03-14 22:01:43 +0500

    hahaha

--- OnlineCinemaProject/OnlineCinemaProject/Models/IdentityModels.cs
+++ OnlineCinemaProject/OnlineCinemaProject/Models/IdentityModels.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity;
-using System.Threading.Tasks;
-using Microsoft.AspNet.Identity;
+﻿using System.Data.Entity;
 using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace OnlineCinemaProject.Models
