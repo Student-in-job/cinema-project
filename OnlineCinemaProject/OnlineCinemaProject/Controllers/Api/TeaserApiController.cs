e47a5704c8d5f2441da720d2fb1a556248617618
ramazan <ramazan.333a@gmail.com>
2016-04-02 17:51:54 +0500

    gg

---
+++ OnlineCinemaProject/OnlineCinemaProject/Controllers/Api/TeaserApiController.cs
@@ -0,0 +1,91 @@
+﻿using Newtonsoft.Json;
+using OnlineCinemaProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web.Http;
+
+
+namespace OnlineCinemaProject.Controllers.Api
+{
+    public class TeaserApiController : ApiController
+            {
+        private OnlineCinemaEntities db = new OnlineCinemaEntities();
+        [HttpGet]
+        [Route("api/teaser/")]
+        public HttpResponseMessage GetTeaser() {
+            db.Configuration.ProxyCreationEnabled = false;
+         
+            // string token = "a9fe59b2-8d88-4bef-87f3-a081bcce6632";
+            //IEnumerable<string> headerValues;
+            //var id_user = string.Empty;
+            //if (Request.Headers.TryGetValues("token", out headerValues))
+            //{
+            //    id_user = headerValues.FirstOrDefault();
+            //}
+            //SELECT teasers.img_url statistics_teaser from teasers where teasers.id not in (SELECT statistics_teaser.id_teasers from statistics_teaser WHERE statistics_teaser.id_user = '9e0cc9f5-0665-49d4-a009-4c859a00b2d9') 
+
+            string query = "SELECT * FROM teaser order by showAmount asc";
+
+            var teaser = db.teasers.SqlQuery(query).ToList().First();
+
+            var sb = db.statistics_teaser.Find(teaser.id);
+
+            db.teasers.Attach(teaser);
+            teaser.showAmount = teaser.showAmount + 1;
+            var entry = db.Entry(teaser);
+            entry.Property(e => e.showAmount).IsModified = true;
+            // other changed properties
+            db.SaveChanges();
+
+            if (sb != null)
+            {
+
+                db.statistics_teaser.Attach(sb);
+                sb.dateShow = DateTime.Now;
+                sb.showAmount = teaser.showAmount;
+                var entry1 = db.Entry(sb);
+                entry1.Property(e => e.showAmount).IsModified = true;
+                entry1.Property(e => e.dateShow).IsModified = true;
+                // other changed properties
+                db.SaveChanges();
+
+
+            }
+            else
+            {
+                sb = new statistics_teaser();
+                sb.dateShow = DateTime.Now;
+                sb.id_teasers = teaser.id;
+                sb.showAmount = teaser.showAmount;
+                sb.id_users = "a9fe59b2-8d88-4bef-87f3-a081bcce6632";
+                db.statistics_teaser.Add(sb);
+                db.SaveChanges();
+            }
+
+           
+
+
+               string strJSON =   Newtonsoft.Json.JsonConvert.SerializeObject(teaser, Formatting.Indented, 
+new JsonSerializerSettings {
+        PreserveReferencesHandling = PreserveReferencesHandling.Objects
+});
+       //     string strJSON = JsonConvert.SerializeObject(teaser,
+       //new EFNavigationPropertyConverter(), new JsonSerializerSettings
+       //{
+       //    PreserveReferencesHandling = PreserveReferencesHandling.Objects
+       //});
+
+           var response = new HttpResponseMessage();
+           response.Content = new StringContent(strJSON);
+           response.Content.Headers.ContentType = new MediaTypeHeaderValue("text/html");
+           return response;
+        }
+
+    }
+}
+
+    
---
+++ OnlineCinemaProject/OnlineCinemaProject/Controllers/StatisticsBannerController.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using OnlineCinemaProject.Models;
+using PagedList;
+namespace OnlineCinemaProject.Controllers
+{
+    [Authorize(Roles = "PRManager ")]
+    public class StatisticsBannerController : Controller
+    {
+      private readonly OnlineCinemaEntities db = new OnlineCinemaEntities();
+         public ActionResult Index(){
+        // GET: /StatisticsBanner/
+        //public ViewResult Index(string sortOrder, string currentFilter, string searchString, int? page)
+        //{
+            var statistics_banner = db.statistics_banner.Include(s => s.aspnetuser).Include(s => s.banner);
+            return View(statistics_banner.ToList());}
+
+        //    //    ViewBag.CurrentSort = sortOrder;
+        //    //    ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+        //    //    ViewBag.DateSortParm = sortOrder == "show_amount" ? "show_amount_desc" : "show_amount";
+        //    //    ViewBag.DateSortParm = sortOrder == "date" ? "date_desc" : "date";
+
+
+        //    //    if (searchString != null)
+        //    //    {
+        //    //        page = 1;
+        //    //    }
+        //    //    else
+        //    //    {
+        //    //        searchString = currentFilter;
+        //    //    }
+
+        //    //    ViewBag.CurrentFilter = searchString;
+
+        //    //    var statistic_banners = from s in db.statistic_banners.
+        //    //                            select s;
+        //    //    if (!String.IsNullOrEmpty(searchString))
+        //    //    {
+        //    //        statistic_banners = statistic_banners.Where(s => s.name.ToUpper().Contains(searchString.ToUpper()));
+
+        //    //    }
+        //    //    switch (sortOrder)
+        //    //    {
+        //    //        case "name_desc":
+        //    //            statistic_banners = statistic_banners.OrderByDescending(s => s.name);
+        //    //            break;
+        //    //        case "show_amount":
+        //    //            statistic_banners = statistic_banners.OrderBy(s => s.show_amount);
+        //    //            break;
+        //    //        case "show_amount_desc":
+        //    //            statistic_banners = statistic_banners.OrderByDescending(s => s.show_amount);
+        //    //            break;
+        //    //        case "date":
+        //    //            statistic_banners = statistic_banners.OrderBy(s => s.date);
+        //    //            break;
+        //    //        case "date_desc":
+        //    //            statistic_banners = statistic_banners.OrderByDescending(s => s.date);
+        //    //            break;
+        //    //        default:  // Name ascending 
+        //    //            statistic_banners = statistic_banners.OrderBy(s => s.name);
+        //    //            break;
+        //    //    }
+
+        //    //    int pageSize = 3;
+        //    //    int pageNumber = (page ?? 1);
+        //    //    return View(statistic_banners.ToPagedList(pageNumber, pageSize));
+        //    //}
+
+        //}
+    }
+}
+// GET: /StatisticsBanner/Details/5
+        //public ActionResult Details(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    statistics_banner statistics_banner = db.statistics_banner.Find(id);
+        //    if (statistics_banner == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(statistics_banner);
+        //}
+
+        //// GET: /StatisticsBanner/Create
+        //public ActionResult Create()
+        //{
+        //    ViewBag.id_user = new SelectList(db.aspnetusers, "Id", "UserName");
+        //    ViewBag.id_banner = new SelectList(db.banners, "id", "name");
+        //    return View();
+        //}
+
+        //// POST: /StatisticsBanner/Create
+        //// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        //// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Create([Bind(Include="id,date,show_amount,id_banner,id_user")] statistics_banner statistics_banner)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        db.statistics_banner.Add(statistics_banner);
+        //        db.SaveChanges();
+        //        return RedirectToAction("Index");
+        //    }
+
+        //    ViewBag.id_user = new SelectList(db.aspnetusers, "Id", "UserName", statistics_banner.id_user);
+        //    ViewBag.id_banner = new SelectList(db.banners, "id", "name", statistics_banner.id_banner);
+        //    return View(statistics_banner);
+        //}
+
+        //// GET: /StatisticsBanner/Edit/5
+        //public ActionResult Edit(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    statistics_banner statistics_banner = db.statistics_banner.Find(id);
+        //    if (statistics_banner == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    ViewBag.id_user = new SelectList(db.aspnetusers, "Id", "UserName", statistics_banner.id_user);
+        //    ViewBag.id_banner = new SelectList(db.banners, "id", "name", statistics_banner.id_banner);
+        //    return View(statistics_banner);
+        //}
+
+        //// POST: /StatisticsBanner/Edit/5
+        //// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        //// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Edit([Bind(Include="id,date,show_amount,id_banner,id_user")] statistics_banner statistics_banner)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        db.Entry(statistics_banner).State = EntityState.Modified;
+        //        db.SaveChanges();
+        //        return RedirectToAction("Index");
+        //    }
+        //    ViewBag.id_user = new SelectList(db.aspnetusers, "Id", "UserName", statistics_banner.id_user);
+        //    ViewBag.id_banner = new SelectList(db.banners, "id", "name", statistics_banner.id_banner);
+        //    return View(statistics_banner);
+        //}
+
+        //// GET: /StatisticsBanner/Delete/5
+        //public ActionResult Delete(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    statistics_banner statistics_banner = db.statistics_banner.Find(id);
+        //    if (statistics_banner == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(statistics_banner);
+        //}
+
+        //// POST: /StatisticsBanner/Delete/5
+        //[HttpPost, ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult DeleteConfirmed(int id)
+        //{
+        //    statistics_banner statistics_banner = db.statistics_banner.Find(id);
+        //    db.statistics_banner.Remove(statistics_banner);
+        //    db.SaveChanges();
+        //    return RedirectToAction("Index");
+        //}
+
+    //    protected override void Dispose(bool disposing)
+    //    {
+    //        if (disposing)
+    //        {
+    //            db.Dispose();
+    //        }
+    //        base.Dispose(disposing);
+    //    }
+    //}
+
