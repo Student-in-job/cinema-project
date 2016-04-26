50ad9b91ad03673b32571b00eed69a8135756927
ramazan <ramazan.333a@gmail.com>
2016-04-26 19:09:16 +0500

    packages and not pushed last time files

---
+++ OnlineCinemaProject/OnlineCinemaProject/Controllers/DashboardController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+
+namespace OnlineCinemaProject.Controllers
+{
+    [Authorize(Roles = "BilingManager, Admin, PRManager, ContentManager")]
+    public class DashboardController : Controller
+    {
+        //
+        // GET: /Dashboard/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
---
+++ OnlineCinemaProject/OnlineCinemaProject/Utils/PaymentChart.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+
+namespace OnlineCinemaProject.Utils
+{
+    public static class DateUtils
+    {
+        public static string GetMonthName(int month)
+        {
+            return CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(month);
+        }
+    }
+
+    public class PaymentTotal
+    {
+        public string MonthName { get; set; }
+        public decimal Total { get; set; }
+    }
+}
