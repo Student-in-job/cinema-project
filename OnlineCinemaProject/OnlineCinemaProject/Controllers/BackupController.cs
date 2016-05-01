4282072cfa6f5e5ea1875ed0228fb22a098675f4
Ivan <ivan.belyavskiy1993@gmail.com>
2016-05-01 22:28:20 +0500

    Сделал backup close #146

---
+++ OnlineCinemaProject/OnlineCinemaProject/Controllers/BackupController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MySql.Data.MySqlClient;
+
+namespace OnlineCinemaProject.Controllers
+{
+    public class BackupController : Controller
+    {
+        //
+        // GET: /Backup/
+        public ActionResult Index()
+        {
+            string constring = "server=localhost;User Id=root;password=22312tuit;database=online-cinema;";
+            string file = "D:\\online-cinema.sql";
+            using (MySqlConnection conn = new MySqlConnection(constring))
+            {
+                using (MySqlCommand cmd = new MySqlCommand())
+                {
+                    using (MySqlBackup mb = new MySqlBackup(cmd))
+                    {
+                        cmd.Connection = conn;
+                        conn.Open();
+                        mb.ExportToFile(file);
+                        conn.Close();
+                    }
+                }
+            }
+
+            return View();
+        }
+    }
+}
